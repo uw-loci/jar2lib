d51a424940861ffa28aaa100a9ba43d8503525ba
Curtis Rueden <ctrueden@wisc.edu>
2010-08-18 20:42:37 +0000

    Set main class appropriately in deps jar.

--- bf-cpp/gen.sh
+++ bf-cpp/gen.sh
@@ -4,9 +4,8 @@
 
 JAR2LIB_DIR=`cd "$(dirname $0)/.." && pwd`
 ARTIFACT_DIR=~/code/LOCI/java/artifacts
-java -cp \
-  "$JAR2LIB_DIR/target/jar2lib-1.0-SNAPSHOT-deps.jar" \
-  loci.jar2lib.Jar2Lib bfcpp "Bio-Formats C++ bindings" \
+java -jar "$JAR2LIB_DIR/target/jar2lib-1.0-SNAPSHOT-deps.jar" \
+  bfcpp "Bio-Formats C++ bindings" \
   $ARTIFACT_DIR/loci-common.jar \
   $ARTIFACT_DIR/ome-xml.jar \
   $ARTIFACT_DIR/bio-formats.jar \
