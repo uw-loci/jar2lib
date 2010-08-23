5353edc94b52532d0918d002faddbb8bec223f34
Curtis Rueden <ctrueden@wisc.edu>
2010-08-23 17:51:45 +0000

    Update bf-cpp invocation script:   * Add loci_plugins library to the mix.   * Point to JARs in local Maven repository.

--- bf-cpp/gen.sh
+++ bf-cpp/gen.sh
@@ -3,12 +3,18 @@
 # NB: Run "mvn assembly:assembly" first to build the complete Jar2Lib JAR file.
 
 JAR2LIB_DIR=`cd "$(dirname $0)/.."; pwd`
-ARTIFACT_DIR=~/code/LOCI/java/artifacts
-java -jar "$JAR2LIB_DIR/target/jar2lib-1.0-SNAPSHOT-deps.jar" \
+REPOSITORY_DIR=~/.m2/repository
+
+java -cp \
+"$JAR2LIB_DIR/target/jar2lib-1.0-SNAPSHOT-deps.jar":\
+"$REPOSITORY_DIR/imagej/ij/1.43/ij-1.43.jar":\
+"$REPOSITORY_DIR/log4j/log4j/1.2.15/log4j-1.2.15.jar" \
+  loci.jar2lib.Jar2Lib \
   bfcpp "Bio-Formats C++ bindings" \
-  $ARTIFACT_DIR/loci-common.jar \
-  $ARTIFACT_DIR/ome-xml.jar \
-  $ARTIFACT_DIR/bio-formats.jar \
-  $ARTIFACT_DIR/flow-cytometry.jar \
+  $REPOSITORY_DIR/loci/loci-common/5.0-SNAPSHOT/loci-common-5.0-SNAPSHOT.jar \
+  $REPOSITORY_DIR/loci/loci_plugins/5.0-SNAPSHOT/loci_plugins-5.0-SNAPSHOT.jar \
+  $REPOSITORY_DIR/loci/ome-xml/5.0-SNAPSHOT/ome-xml-5.0-SNAPSHOT.jar \
+  $REPOSITORY_DIR/loci/bio-formats/5.0-SNAPSHOT/bio-formats-5.0-SNAPSHOT.jar \
+  $REPOSITORY_DIR/loci/flow-cytometry/1.0-SNAPSHOT/flow-cytometry-1.0-SNAPSHOT.jar \
   -conflicts $JAR2LIB_DIR/bf-cpp/conflicts.txt \
   -header $JAR2LIB_DIR/bf-cpp/header.txt
