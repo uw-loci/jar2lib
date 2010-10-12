3eba5fc04a83797f08fb48afb9dc3a0a89991618
Curtis Rueden <ctrueden@wisc.edu>
2010-10-12 21:34:46 +0000

    Minor tweaks.

--- bf-cpp/gen.sh
+++ bf-cpp/gen.sh
@@ -2,7 +2,8 @@
 
 # NB: Run "mvn assembly:assembly" first to build the complete Jar2Lib JAR file.
 
-JAR2LIB_DIR=`cd "$(dirname $0)/.."; pwd`
+PROJECT_DIR=`cd "$(dirname $0)"; pwd`
+JAR2LIB_DIR=`cd "$PROJECT_DIR/.."; pwd`
 REPOSITORY_DIR=~/.m2/repository
 
 java -cp \
@@ -11,10 +12,10 @@ java -cp \
 "$REPOSITORY_DIR/log4j/log4j/1.2.15/log4j-1.2.15.jar" \
   loci.jar2lib.Jar2Lib \
   bfcpp "Bio-Formats C++ bindings" \
-  $REPOSITORY_DIR/loci/loci-common/5.0-SNAPSHOT/loci-common-5.0-SNAPSHOT.jar \
-  $REPOSITORY_DIR/loci/loci_plugins/5.0-SNAPSHOT/loci_plugins-5.0-SNAPSHOT.jar \
-  $REPOSITORY_DIR/loci/ome-xml/5.0-SNAPSHOT/ome-xml-5.0-SNAPSHOT.jar \
-  $REPOSITORY_DIR/loci/bio-formats/5.0-SNAPSHOT/bio-formats-5.0-SNAPSHOT.jar \
-  $REPOSITORY_DIR/loci/flow-cytometry/1.0-SNAPSHOT/flow-cytometry-1.0-SNAPSHOT.jar \
-  -conflicts $JAR2LIB_DIR/bf-cpp/conflicts.txt \
-  -header $JAR2LIB_DIR/bf-cpp/header.txt
+  "$REPOSITORY_DIR/loci/loci-common/5.0-SNAPSHOT/loci-common-5.0-SNAPSHOT.jar" \
+  "$REPOSITORY_DIR/loci/loci_plugins/5.0-SNAPSHOT/loci_plugins-5.0-SNAPSHOT.jar" \
+  "$REPOSITORY_DIR/loci/ome-xml/5.0-SNAPSHOT/ome-xml-5.0-SNAPSHOT.jar" \
+  "$REPOSITORY_DIR/loci/bio-formats/5.0-SNAPSHOT/bio-formats-5.0-SNAPSHOT.jar" \
+  "$REPOSITORY_DIR/loci/flow-cytometry/1.0-SNAPSHOT/flow-cytometry-1.0-SNAPSHOT.jar" \
+  -conflicts "$PROJECT_DIR/conflicts.txt" \
+  -header "$PROJECT_DIR/header.txt"
